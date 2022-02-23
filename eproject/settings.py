8cb562ac337cada1d3ccf58295bda29bedf2560b
Avez Qureshi <avezqureshi14@gmail.com>
2022-02-23 13:29:14 +0530

    white3

--- eproject/settings.py
+++ eproject/settings.py
@@ -130,3 +130,4 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
