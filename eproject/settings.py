f4e7fd7c259b19d33622546e6a8857a0ca5a4563
Avez Qureshi <avezqureshi14@gmail.com>
2022-02-23 13:22:56 +0530

    whitenoise

--- eproject/settings.py
+++ eproject/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -117,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
 
